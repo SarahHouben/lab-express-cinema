cbefbcc01c007844e3707e38212e013f3cca2cde
SarahHouben <sarahhouben@outlook.com>
2019-09-09 16:19:40 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
-const router  = express.Router();
+const Movie = require('../models/Movie')
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+/* GET all the movies */
+router.get('/movies', (req, res, next) => {
+
+  Movie.find().then(movies => {
+
+    res.render('movies', {
+      moviesList: movies
+    });
+  })
+});
+
+router.get('/movies/:movieId', (req, res) => {
+  const movieId = req.params.movieId;
+
+  Movie.findById(movieId).then(movie => {
+    res.render('movieDetails', {
+      movie: movie
+    });
+  });
 });
 
 module.exports = router;
